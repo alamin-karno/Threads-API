48898417b52141c1efb73987b12b9b54d5e94c42
alamin-karno <amin15-1951@diu.edu.bd>
2023-07-11 00:53:51 +0600

    ThreadsAPI: login API done

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use App\Http\Requests\RegisterRequest;
 use Exception;
@@ -28,6 +29,37 @@ class AuthenticationController extends Controller
                 'user' => $user, 
                 'token' => $token
             ],201);
+        }
+        catch(Exception $e){
+            return response([
+                'message' => $e->getMessage(),
+            ],500);
+        }
+    }
+
+    public function login(LoginRequest $loginRequest){
+        try{
+            $loginRequest->validated();
+
+            $user = User::whereUsername($loginRequest->username)->first();
+
+            if(!$user){
+                return response([
+                    'message' => 'Invalid username',
+                ],422);
+            }
+            elseif(!Hash::check($loginRequest->password, $user->password)){
+                return response([
+                    'message' => 'Password is incorrect',
+                ],422);
+            }
+
+            $token = $user->createToken('threads')->plainTextToken;
+
+            return response([
+                'user' => $user, 
+                'token' => $token
+            ],200);
         }
         catch(Exception $e){
             return response([
