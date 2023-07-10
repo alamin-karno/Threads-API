6536be13dbb83416a11dfdf99561178b5f2c0b26
alamin-karno <amin15-1951@diu.edu.bd>
2023-07-11 00:40:16 +0600

    ThreadsAPI: registration API done

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Http\Requests\RegisterRequest;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
     public function register(RegisterRequest $registerRequest){
         try{
             $registerRequest->validated();
+
+            $user = User::create([
+                'name' => $registerRequest->name,
+                'username' => $registerRequest->username,
+                'email' => $registerRequest->email,
+                'password' => Hash::make($registerRequest->password)
+            ]);
+
+            $token = $user->createToken('threads')->plainTextToken;
+
+            return response([
+                'user' => $user, 
+                'token' => $token
+            ],201);
         }
         catch(Exception $e){
             return response([
